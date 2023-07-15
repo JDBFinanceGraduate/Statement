08fb0f35be3d0f321b362294fafc39989fb65f37
JDBFinanceGraduate <124105570+JDBFinanceGraduate@users.noreply.github.com>
2023-07-14 20:52:24 -0500

    Update state_descr_edit.sql

--- state_descr_edit.sql
+++ state_descr_edit.sql
@@ -1,3 +1,5 @@
 UPDATE statev2 
 set description = 'Twitch'
 where description like '%Twitch%'
+
+--Updating each transaction to have a shorter description through the use of a conditional SQL Script.
